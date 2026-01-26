c57c74992f2bd99f07e3c435359815249f104b6c
LILYGO_L <13073925292@163.com>
2026-01-26 14:28:42 +0800

    add t-spe device

---
+++ src/device/t_spe/t_spe_driver.h
@@ -0,0 +1,11 @@
+/*
+ * @Description: t_spe_driver
+ * @Author: LILYGO_L
+ * @Date: 2026-01-26 09:56:22
+ * @LastEditTime: 2026-01-26 09:57:11
+ * @License: GPL 3.0
+ */
+
+#pragma once
+
+#include "t_spe_config.h"
--- src/lilygo_device_driver_library.h
+++ src/lilygo_device_driver_library.h
@@ -2,7 +2,7 @@
  * @Description: None
  * @Author: LILYGO_L
  * @Date: 2025-12-19 10:32:32
- * @LastEditTime: 2026-01-22 11:39:26
+ * @LastEditTime: 2026-01-26 10:43:52
  * @License: GPL 3.0
  */
 
@@ -15,6 +15,9 @@
 
 #elif defined CONFIG_DEVICE_T_DISPLAY_P4_AIR
 #include "t_display_p4_air_driver.h"
+
+#elif defined CONFIG_DEVICE_T_SPE
+#include "t_spe_driver.h"
 
 #else
 #error "no macro definition is set"
