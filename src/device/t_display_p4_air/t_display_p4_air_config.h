c0e5677c89af874f006189c50890c44363b91c5b
LILYGO_L <13073925292@163.com>
2026-03-03 16:33:13 +0800

    add XL9555_USBPHY_POWER_EN

--- src/device/t_display_p4_air/t_display_p4_air_config.h
+++ src/device/t_display_p4_air/t_display_p4_air_config.h
@@ -2,7 +2,7 @@
  * @Description: t_display_p4_air_config
  * @Author: LILYGO_L
  * @Date: 2026-01-22 09:15:30
- * @LastEditTime: 2026-03-03 16:25:33
+ * @LastEditTime: 2026-03-03 16:32:58
  * @License: GPL 3.0
  */
 
@@ -29,6 +29,7 @@
 // XL9555引脚功能
 #define XL9555_BHI260AP_RST Cpp_Bus_Driver::Xl95x5::Pin::IO2
 #define XL9555_LR1121_POWER_EN Cpp_Bus_Driver::Xl95x5::Pin::IO5
+#define XL9555_USBPHY_POWER_EN Cpp_Bus_Driver::Xl95x5::Pin::IO10
 #define XL9555_ESP32P4_ESP32C5_UART_SWITCH Cpp_Bus_Driver::Xl95x5::Pin::IO11
 #define XL9555_ESP32C5_EN Cpp_Bus_Driver::Xl95x5::Pin::IO12
 #define XL9555_TOUCH_RST Cpp_Bus_Driver::Xl95x5::Pin::IO13
