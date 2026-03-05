3c0fc35061769df7b58b17637cb333f49e0c1069
LILYGO_L <13073925292@163.com>
2026-03-05 15:37:55 +0800

    add new device T-Display-P4

--- src/lilygo_device_driver_library.h
+++ src/lilygo_device_driver_library.h
@@ -2,7 +2,7 @@
  * @Description: None
  * @Author: LILYGO_L
  * @Date: 2025-12-19 10:32:32
- * @LastEditTime: 2026-01-26 10:43:52
+ * @LastEditTime: 2026-03-05 14:18:11
  * @License: GPL 3.0
  */
 
@@ -18,6 +18,9 @@
 
 #elif defined CONFIG_DEVICE_T_SPE
 #include "t_spe_driver.h"
+
+#elif defined CONFIG_DEVICE_T_DISPLAY_P4
+#include "t_display_p4_driver.h"
 
 #else
 #error "no macro definition is set"
