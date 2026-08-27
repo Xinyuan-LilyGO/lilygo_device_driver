223154f502585419bd0f0801a6e780fc799c57a3
LILYGO_L <135582120+Llgok@users.noreply.github.com>
2026-08-27 16:22:52 +0800

    feat(t-can485-c5): update board pin and peripheral definitions
    - remap CAN, RS485, ADC, and WS2812 pins
    - define shared SPI bus pin assignments
    - add W5500 CS, interrupt, and reset pins
    - add isolated digital input configuration
    - add ADC input scaling and W5500 PHY settings

--- src/device/t_can485_c5/t_can485_c5_config.h
+++ src/device/t_can485_c5/t_can485_c5_config.h
@@ -2,7 +2,7 @@
  * @Description: T-CAN485-C5 板级硬件配置
  * @Author: LILYGO_L
  * @Date: 2026-06-12 12:00:00
- * @LastEditTime: 2026-06-12 17:56:48
+ * @LastEditTime: 2026-08-26 12:00:00
  * @License: GPL 3.0
  */
 
@@ -11,44 +11,68 @@
 namespace lilygo_device_driver::t_can485_c5 {
 namespace gpio {
 namespace can {
-inline constexpr int kTx = 0;
-inline constexpr int kRx = 1;
+inline constexpr int kTx = 8;
+inline constexpr int kRx = 9;
 }  // namespace can
 
-namespace sd {
+namespace spi {
 inline constexpr int kMiso = 2;
 inline constexpr int kMosi = 7;
 inline constexpr int kSclk = 6;
+}  // namespace spi
+
+namespace sd {
+inline constexpr int kMiso = spi::kMiso;
+inline constexpr int kMosi = spi::kMosi;
+inline constexpr int kSclk = spi::kSclk;
 inline constexpr int kCs = 10;
 }  // namespace sd
 
 namespace adc {
-inline constexpr int kChannel1 = 3;
-inline constexpr int kChannel2 = 5;
-inline constexpr int kChannel3 = 4;
+inline constexpr int kVoltage = 4;
 }  // namespace adc
 
 namespace rs485 {
-inline constexpr int kTx = 8;
-inline constexpr int kRx = 9;
+inline constexpr int kTx = 0;
+inline constexpr int kRx = 1;
 }  // namespace rs485
 
 namespace button {
 inline constexpr int kEsp32c5Boot = 28;
 }  // namespace button
 
-namespace led {
-inline constexpr int kNumber1 = 26;
-inline constexpr int kNumber2 = 24;
-inline constexpr int kNumber3 = 25;
-}  // namespace led
-
 namespace ws2812 {
-inline constexpr int kData = 23;
+inline constexpr int kData = 3;
 }  // namespace ws2812
+
+namespace digital_input {
+inline constexpr int kOptocoupler = 24;
+}  // namespace digital_input
+
+namespace w5500 {
+inline constexpr int kMiso = spi::kMiso;
+inline constexpr int kMosi = spi::kMosi;
+inline constexpr int kSclk = spi::kSclk;
+inline constexpr int kCs = 23;
+inline constexpr int kInterrupt = 27;
+inline constexpr int kReset = 5;
+}  // namespace w5500
 }  // namespace gpio
 
 namespace device {
+namespace adc {
+inline constexpr int kInputScaleNumerator = 526;
+inline constexpr int kInputScaleDenominator = 56;
+}  // namespace adc
+
+namespace digital_input {
+inline constexpr int kActiveLevel = 0;
+}  // namespace digital_input
+
+namespace w5500 {
+inline constexpr int kPhyAddress = 0;
+}  // namespace w5500
+
 namespace ws2812 {
 inline constexpr int kLedCount = 1;
 }  // namespace ws2812
